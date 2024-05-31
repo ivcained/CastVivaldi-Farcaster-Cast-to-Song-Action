122727f53f85ffe25f095e859444f402985712bc
ivcained <86070833+ivcained@users.noreply.github.com>
2024-05-31 15:21:52 +0530

    chore: else

--- api/index.tsx
+++ api/index.tsx
@@ -66,109 +66,109 @@ app.hono.post("/api", async (c) => {
   // validate the cast action
   const { isValid, message } = await validateFramesMessage(body);
   const interactorFid = message?.data?.fid;
-  const castFid = message?.data.frameActionBody.castId;//?.fid;
+  const castFid = message?.data.frameActionBody.castId; //?.fid;
   const hash = hexStringToBytes(
     base64FromBytes(castFid?.hash as Uint8Array)
   )._unsafeUnwrap();
   //const hash = hexStringToBytes(base64FromBytes(castFid?.hash as Uint8Array));
-    // generate music based on the text in the cast
-    const text =
-      (body.data && body.data?.text) ||
-      "castvivaldi turns casts into songs on Farcaster"; // Send the text in the cast - Test here is killing me.
-    console.log("Text to be sent:", text); // Log the text to be sent
-    const response = await fetch(process.env.AUDIO_GEN_API as string, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        prompt: text || "", // Use an empty string as the default value if 'text' is undefined
-        tags: "rock, pop",
-        title: "Cast Vivaldi",
-        make_instrumental: false,
-        wait_audio: true,
-      }), // send the text as the body of the request
-    });
-    // Convert the hash to JSON format
-    // Parse the JSON PROD
-    const musicDataArray = (await response.json()) as musicData[];
-    const musicData = musicDataArray[0];
+  // generate music based on the text in the cast
+  const text =
+    (body.data && body.data?.text) ||
+    "castvivaldi turns casts into songs on Farcaster"; // Send the text in the cast - Test here is killing me.
+  console.log("Text to be sent:", text); // Log the text to be sent
+  const response = await fetch(process.env.AUDIO_GEN_API as string, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      prompt: text || "", // Use an empty string as the default value if 'text' is undefined
+      tags: "rock, pop",
+      title: "Cast Vivaldi",
+      make_instrumental: false,
+      wait_audio: true,
+    }), // send the text as the body of the request
+  });
+  // Convert the hash to JSON format
+  // Parse the JSON PROD
+  const musicDataArray = (await response.json()) as musicData[];
+  const musicData = musicDataArray[0];
 
-    //const musicData = await response.json(); // get the response data na
-    // create the cast informing initiation reply under the thread na
-    const castReplyResult = await makeCastAdd(
-      {
-        text: "Generating Song...",
-        embeds: [], //[{ url: musicData.audio_url }], // add music URL here
-        embedsDeprecated: [],
-        mentions: [],
-        mentionsPositions: [], // need to add FID mentions position
-        parentCastId: {
-          fid: castFid?.fid as number,
-          hash: hash, // hash
-        },
+  //const musicData = await response.json(); // get the response data na
+  // create the cast informing initiation reply under the thread na
+  const castReplyResult = await makeCastAdd(
+    {
+      text: "Generating Song...",
+      embeds: [], //[{ url: musicData.audio_url }], // add music URL here
+      embedsDeprecated: [],
+      mentions: [],
+      mentionsPositions: [], // need to add FID mentions position
+      parentCastId: {
+        fid: castFid?.fid as number,
+        hash: hash, // hash
       },
-      dataOptions,
-      ed25519Signer
-    );
-    // Wait for the first cast to be confirmed before proceeding
-    await new Promise((resolve) => setTimeout(resolve, 3000)); // Delay to allow the cast to confirm
-    // create the cast reply under the thread
-    await makeCastAdd(
-      {
-        text: "▶︎ •၊၊||၊|။|||။|||။|||။၊|.Here's your Song!",
-        embeds: [
-          { url: musicData.audio_url }, // audio URL here uses the variable's value
-        ],
-        embedsDeprecated: [],
-        mentions: [], // need to add FID mentions position
-        mentionsPositions: [], // need to add FID mentions position
-        parentCastId: {
-          fid: castFid?.fid as number,
-          hash: hash,
-        },
+    },
+    dataOptions,
+    ed25519Signer
+  );
+  // Wait for the first cast to be confirmed before proceeding
+  await new Promise((resolve) => setTimeout(resolve, 3000)); // Delay to allow the cast to confirm
+  // create the cast reply under the thread
+  await makeCastAdd(
+    {
+      text: "▶︎ •၊၊||၊|။|||။|||။|||။၊|.Here's your Song!",
+      embeds: [
+        { url: musicData.audio_url }, // audio URL here uses the variable's value
+      ],
+      embedsDeprecated: [],
+      mentions: [], // need to add FID mentions position
+      mentionsPositions: [], // need to add FID mentions position
+      parentCastId: {
+        fid: castFid?.fid as number,
+        hash: hash,
       },
-      dataOptions,
-      ed25519Signer
-    );
-    // Create a client instance
+    },
+    dataOptions,
+    ed25519Signer
+  );
+  // Create a client instance
 
-    const client = getSSLHubRpcClient("hubs-grpc.airstack.xyz");
+  const client = getSSLHubRpcClient("hubs-grpc.airstack.xyz");
 
-    client.$.waitForReady(Date.now() + 5000, async (e) => {
-      if (e) {
-        console.error(`Failed to connect to the gRPC server:`, e);
-        process.exit(1);
-      } else {
-        const metadata = new Metadata();
-        // Provide API key here
-        metadata.add("x-airstack-hubs", process.env.AIRSTACK_API_KEY as string);
-        if (castReplyResult.isOk()) {
-          // broadcast the cast throughout the Farcaster network
-          const castAddMessage = castReplyResult.value;
-          
-          const submitResult = await client.submitMessage(
-            castAddMessage,
-            metadata
-          );
-          if (submitResult.isOk()) {
-            console.log(`Reply posted successfully`);
-            console.log(Buffer.from(submitResult.value.hash).toString("hex"));
-          }
-        } else {
-          const error = castReplyResult.error;
-          // Handle the error case
-          console.error(`Error posting reply: ${error}`);
+  client.$.waitForReady(Date.now() + 5000, async (e) => {
+    if (e) {
+      console.error(`Failed to connect to the gRPC server:`, e);
+      process.exit(1);
+    } else {
+      const metadata = new Metadata();
+      // Provide API key here
+      metadata.add("x-airstack-hubs", process.env.AIRSTACK_API_KEY as string);
+      if (castReplyResult.isOk()) {
+        // broadcast the cast throughout the Farcaster network
+        const castAddMessage = castReplyResult.value;
+
+        const submitResult = await client.submitMessage(
+          castAddMessage,
+          metadata
+        );
+        if (submitResult.isOk()) {
+          console.log(`Reply posted successfully`);
+          console.log(Buffer.from(submitResult.value.hash).toString("hex"));
         }
-        // After everything, close the RPC connection
-        client.close();
+      } else {
+        const error = castReplyResult.error;
+        // Handle the error case
+        console.error(`Error posting reply: ${error}`);
       }
-    });
-    // This will be the message appearing in the cast action toast, customizable
-    return c.json({ message: "Yay, Success" });
-  } else {
+      // After everything, close the RPC connection
+      client.close();
+    }
+  });
+  // This will be the message appearing in the cast action toast, customizable
+  return c.json({ message: "Yay, Success" });
+  /*} else {
     return c.json({ message: "Uh Oh .. Unauthorized" }, 401);
-  }
+  }*/
 });
 devtools(app, { serveStatic });
 export const GET = handle(app);
